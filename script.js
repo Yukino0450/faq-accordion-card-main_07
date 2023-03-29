cbf60c002772d8aacbe25978c6a1e1404499b481
Yukino0450 <ymy4500@gmail.com>
2023-03-30 04:57:08 +0900

    completed the project

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-function addAppear(answerNumber, arrowNumber) {
+function addAppear(answerNumber, arrowNumber, questionNumber) {
   const questionElement = document.getElementById(answerNumber);
   const arrowElement = document.getElementById(arrowNumber);
+  const boldElement = document.getElementById(questionNumber);
   if (questionElement.classList.contains("appear")) {
     questionElement.classList.remove("appear");
     arrowElement.classList.remove("upSideDown");
+    boldElement.classList.remove("fontWeight");
   } else {
     questionElement.classList.add("appear");
     arrowElement.classList.add("upSideDown");
+    boldElement.classList.add("fontWeight");
   }
   console.log("clicked question");
 }
